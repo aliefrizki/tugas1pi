32ad2075c21473df485850d2e8bd8d7f1c70b161
aliefrizki <80031904+aliefrizki@users.noreply.github.com>
2021-04-02 19:49:04 +0700

    pembuatan form sederhana

---
+++ form.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+
+
+<div class="container">
+  <div class="col-8">
+  <h1>Form Pendaftarn Sekte aqua</h1>
+  <form>
+    <div class="form-group">
+      <label for="Nama">Nama</label>
+      <input type="text" class="form-control" id="Nama" placeholder="Enter name">
+    </div>
+
+    <div class="form-group">
+      <label for="username">username</label>
+      <input type="text" class="form-control" id="username" placeholder="username">
+    </div>
+
+    <div class="form-group">
+      <label for="username">Suku</label>
+      <input type="text" class="form-control" id="Suku" placeholder="Suku">
+    </div>
+
+    <div class="form-group">
+      <label for="username">Agama</label>
+      <select class="custom-select">
+        <option selected>Pilih agama</option>
+        <option value="islam">Islam</option>
+        <option value="kristen">Kristen</option>
+        <option value="kristen">Katolik</option>
+        <option value="budha">budha</option>
+        <option value="hindu">hindu</option>
+        <option value="konghucu">konghucu</option>
+        <option value="army">Army</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+      <label for="username">jenis kelamin</label>
+      <select class="custom-select">
+        <option selected>Pilih jenis kelamin</option>
+        <option value="lakilaki">Laki-laki</option>
+        <option value="perempuan">perempuan</option>
+        <option value="nonbinary">non binary</option>   
+    </select>
+    </div>
+
+
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+</body>
+</html>
